12f19770a1f1eca70d04f6a6a97865185bef3d68
Erica Leff <ericaleff@gmail.com>
2018-04-29 01:19:24 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,19 @@
+def turn_count(board)
+  turns = 0
+  board.each do |taken|
+    if taken == "X" || taken == "O"
+      turns += 1
+    end
+end
+
+turns
+end
+
+def current_player(board)
+  if turn_count % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+
+end
